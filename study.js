a9736a67be84af274b3d9e804fe87491c786a156
SUBHRANIL BHATTACHARYYA <subhranilb18@gmail.com>
2024-04-15 21:34:14 +0530

    Initial commit

---
+++ study.js
@@ -0,0 +1,10 @@
+class User{
+    name = prompt();
+    email = prompt();
+    viewData(name , email){
+        console.log(<a href= ""></a>);
+    }
+}
+
+let obj = new User(Dipai , subhranilb18@gmail.com); 
+
